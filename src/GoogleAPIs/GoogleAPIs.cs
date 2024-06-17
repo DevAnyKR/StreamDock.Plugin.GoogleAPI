7ef4aa03262c380e00f91938425881cae7f0fd65
DevAny <devany.kr@gmail.com>
2024-06-17 14:49:16 +0900

    잘못된 클래스, 상속 구조 변경

--- src/GoogleAPIs/GoogleAPIs.cs
+++ src/GoogleAPIs/GoogleAPIs.cs
@@ -1,11 +1,4 @@
-﻿using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
-
-using BarRaider.SdTools;
-
-using Google.Apis.Adsense.v2;
-using Google.Apis.Auth.OAuth2;
+﻿using Google.Apis.Auth.OAuth2;
 
 namespace StreamDock.Plugins.GoogleAPIs
 {
@@ -14,29 +7,11 @@ namespace StreamDock.Plugins.GoogleAPIs
     /// </summary>
     internal abstract class GoogleAPI
     {
+        protected const string googleClientSecretsJson = "client_secrets.json";
+
         internal GoogleAPI()
         {
             GoogleWebAuthorizationBroker.Folder = @"DevAny\StreamDock.Plugins\GoogleAPI";
-        }
-
-        /// <summary>
-        /// Client Secret 정보를 불러옵니다.
-        /// </summary>
-        /// <returns></returns>
-        protected async Task<UserCredential> GetClientSecretAsync()
-        {
-            UserCredential credential;
-
-            using (var stream = new FileStream("client_secrets.json", FileMode.Open, FileAccess.Read))
-            {
-                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.FromStream(stream).Secrets,
-                    new[] { AdsenseService.Scope.AdsenseReadonly },
-                    "user", CancellationToken.None);
-            }
-            Logger.Instance.LogMessage(TracingLevel.INFO, "Read client_secrets.json");
-
-            return credential;
         }
     }
 }
